2b24d7c396dc7965b3ac2e60c3b31703025a5d10
Helcio Anicio <helcioanicio19@gmail.com>
2023-02-13 23:04:21 -0300

    Fixed loop For

--- src/script.js
+++ src/script.js
@@ -5,44 +5,13 @@ const divs = document.querySelectorAll('.div_informations');
 
 for (let index = 0; index < divs.length; index++) {
     divs[index].addEventListener('click', ()=>{
-        // questions = divs[index]
-        if (divs.classList.contains('show')) {
+        questions = divs[index]
+        if (divs[index].classList.contains('show')) {
             arrows.classList.remove('arrow_rotate');
-            divs.classList.remove('show');
+            divs[index].classList.remove('show');
         } else {
             arrows.classList.add('arrow_rotate');
-            divs.classList.add('show');
+            divs[index].classList.add('show');
         }
     });
 };
-
-/*
-divs.forEach((div) => 
-div.addEventListener('click', function(){
-            if (div.classList.contains('show')) {
-                arrows.classList.remove('arrow_rotate');
-                div.classList.remove('show');
-            } else {
-                arrows.classList.add('arrow_rotate');
-                div.classList.add('show');
-            }
-        })
-        );
-*/
-        
-
-/* 
-let feedback_level = 0
-
-const inputs = document.querySelectorAll('input');
-for (let index = 0; index < inputs.length; index++) {
-    inputs[index].addEventListener('change', ()=>{
-            feedback_level = inputs[index].value
-            document.querySelector('button').addEventListener('click', ()=>{
-                document.querySelectorAll('main')[0].style.display='none';
-                document.querySelectorAll('section')[0].style.display='flex';
-                document.querySelector('span').innerHTML=feedback_level
-            })
-        })
-    }
-s*/
