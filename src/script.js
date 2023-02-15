a2fd43a41c46d70972b4185140dc8e2ee64f61ce
Helcio Anicio <helcioanicio19@gmail.com>
2023-02-14 23:17:14 -0300

    trying use conditional on more divs

--- src/script.js
+++ src/script.js
@@ -3,6 +3,33 @@ let questions = 0;
 const arrows = document.querySelector('.arrow');
 const divs = document.querySelectorAll('.div_informations');
 
+// let close = function () {
+//     arrows.
+// }
+
+for (let index = 0; index < divs.length; index++) {
+        divs[index].addEventListener('click', ()=>{
+            questions = divs[index]
+            if (divs[index].classList.contains('show')) {
+                arrows.classList.remove('arrow_rotate');
+                divs[index].classList.remove('show');
+            } else {
+                arrows.classList.add('arrow_rotate');
+                divs[index].classList.add('show');
+            }            
+            
+        });
+    };
+
+
+
+
+/*
+let questions = 0;
+
+const arrows = document.querySelector('.arrow');
+const divs = document.querySelectorAll('.div_informations');
+
 for (let index = 0; index < divs.length; index++) {
     divs[index].addEventListener('click', ()=>{
         questions = divs[index]
@@ -15,3 +42,4 @@ for (let index = 0; index < divs.length; index++) {
         }
     });
 };
+*/
