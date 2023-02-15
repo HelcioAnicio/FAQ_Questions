37f5751031ef2582b6cd2aa9963bc0104edf3289
Helcio Anicio <helcioanicio19@gmail.com>
2023-02-15 10:54:51 -0300

    Fixed the problem with arrow

--- src/script.js
+++ src/script.js
@@ -1,45 +1,53 @@
 let questions = 0;
 
-const arrows = document.querySelector('.arrow');
 const divs = document.querySelectorAll('.div_informations');
 
-// let close = function () {
-//     arrows.
-// }
-
-for (let index = 0; index < divs.length; index++) {
-        divs[index].addEventListener('click', ()=>{
-            questions = divs[index]
-            if (divs[index].classList.contains('show')) {
-                arrows.classList.remove('arrow_rotate');
-                divs[index].classList.remove('show');
+divs.forEach((div) => {
+        div.addEventListener('click',() => {
+            if (div.classList.contains('show','arrow_rotate')) {
+                div.classList.remove('show','arrow_rotate')
             } else {
-                arrows.classList.add('arrow_rotate');
-                divs[index].classList.add('show');
-            }            
-            
-        });
-    };
-
+                div.classList.add('show','arrow_rotate')
+            }
+    });
+});
 
 
 
 /*
-let questions = 0;
-
-const arrows = document.querySelector('.arrow');
-const divs = document.querySelectorAll('.div_informations');
-
-for (let index = 0; index < divs.length; index++) {
-    divs[index].addEventListener('click', ()=>{
-        questions = divs[index]
-        if (divs[index].classList.contains('show')) {
-            arrows.classList.remove('arrow_rotate');
-            divs[index].classList.remove('show');
+divs.forEach((div) => {
+    div.addEventListener('mouseleave', () => {
+        div.classList.remove('show', 'arrow_rotate');
+    })
+    div.addEventListener('click', () => {
+        if (div.classList.contains('show', 'arrow_rotate')) {
+            
         } else {
-            arrows.classList.add('arrow_rotate');
-            divs[index].classList.add('show');
+            div.classList.add('show','arrow_rotate');
         }
     });
-};
+});
 */
+
+
+
+
+
+
+// let questions = 0;
+
+// const arrows = document.querySelectorAll('.arrow');
+// const divs = document.querySelectorAll('.div_informations');
+
+// for (let index = 0; index < divs.length; index++) {
+//     divs[index].addEventListener('click', ()=>{
+//         questions = divs[index]
+//         if (divs[index].classList.contains('show')) {
+//             arrows[index].classList.remove('arrow_rotate');
+//             divs[index].classList.remove('show');
+//         } else {
+//             arrows[index].classList.add('arrow_rotate');
+//             divs[index].classList.add('show');
+//         }
+//     });
+// };
