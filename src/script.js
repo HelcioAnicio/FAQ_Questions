c928cc16101d8263b1b976577c3b4b8212f28e28
Helcio Anicio <helcioanicio19@gmail.com>
2023-02-15 22:24:16 -0300

    Finished JS

--- src/script.js
+++ src/script.js
@@ -1,53 +1,12 @@
-let questions = 0;
-
 const divs = document.querySelectorAll('.div_informations');
-
 divs.forEach((div) => {
         div.addEventListener('click',() => {
             if (div.classList.contains('show','arrow_rotate')) {
                 div.classList.remove('show','arrow_rotate')
-            } else {
-                div.classList.add('show','arrow_rotate')
+            
+            } else { 
+                divs.forEach(div => div.classList.remove('show','arrow_rotate'))
+                 div.classList.add('show','arrow_rotate')
             }
     });
 });
-
-
-
-/*
-divs.forEach((div) => {
-    div.addEventListener('mouseleave', () => {
-        div.classList.remove('show', 'arrow_rotate');
-    })
-    div.addEventListener('click', () => {
-        if (div.classList.contains('show', 'arrow_rotate')) {
-            
-        } else {
-            div.classList.add('show','arrow_rotate');
-        }
-    });
-});
-*/
-
-
-
-
-
-
-// let questions = 0;
-
-// const arrows = document.querySelectorAll('.arrow');
-// const divs = document.querySelectorAll('.div_informations');
-
-// for (let index = 0; index < divs.length; index++) {
-//     divs[index].addEventListener('click', ()=>{
-//         questions = divs[index]
-//         if (divs[index].classList.contains('show')) {
-//             arrows[index].classList.remove('arrow_rotate');
-//             divs[index].classList.remove('show');
-//         } else {
-//             arrows[index].classList.add('arrow_rotate');
-//             divs[index].classList.add('show');
-//         }
-//     });
-// };
